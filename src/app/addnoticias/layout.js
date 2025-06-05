88ceabe00e3a787efb5ecc2b753a0a17dd500e37
David_Martinez <davidmartinez5732891@gmail.com>
2025-06-05 12:18:28 -0600

    it was added an example of noticias page and I added a layout page in order to show a unique layout

---
+++ src/app/addnoticias/layout.js
@@ -0,0 +1,30 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "../globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Agregar noticias",
+  description: "puedes agregar noticias a tu sitio web",
+  keywords: "noticias, agregar, sitio web, contenido",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
