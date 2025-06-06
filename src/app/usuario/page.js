96e6a3a481e1b97a72ff5eff0859039121eb291b
David_Martinez <davidmartinez5732891@gmail.com>
2025-06-06 09:08:27 -0600

    se agrego un ejemplo de la pagina usuario

---
+++ src/app/usuario/page.js
@@ -0,0 +1,26 @@
+import image from 'next/image';
+
+export default function Usuario() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <p className="text-2xl font-bold">
+          Usuario
+        </p>
+        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+          <li className="mb-2 tracking-[-.01em]">
+            Aquí puedes gestionar tu perfil de usuario.
+          </li>
+          <li className="tracking-[-.01em]">
+            Actualiza tu información personal y preferencias.
+          </li>
+        </ol>
+      </main>
+      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+        <p className="text-sm text-gray-500">
+          © 2023 Tu Sitio Web. Todos los derechos reservados.
+        </p>
+      </footer>
+    </div>
+  );
+}
