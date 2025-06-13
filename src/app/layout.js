ba7c4acf190b7f87cd1c7aae1fe2032ed5b78cb1
David_Martinez <davidmartinez5732891@gmail.com>
2025-06-13 10:04:56 -0600

    se agregaron los tipos de letra

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,19 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from '@/app/components/Navbar.js';
+import { Quicksand, Roboto } from 'next/font/google';
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+const quicksand = Quicksand({
+  variable: '--font-quicksand',
+  subsets: ['latin'],
+  weight: ['400', '500', '700'],
+  display: 'swap',
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const roboto = Roboto({
+  variable: '--font-roboto',
+  subsets: ['latin'],
+  weight: ['400', '500', '700'],
+  display: 'swap',
 });
 
 export const metadata = {
@@ -27,9 +31,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className="antialiased">
         <Navbar className="top-0 z-50 bg-black"/>
         <main className="mt-[72px]">{children}</main>
       </body>
