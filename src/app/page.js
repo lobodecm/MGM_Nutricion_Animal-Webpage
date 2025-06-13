4e875688ed2002ee8654fdcb53914818b2b82d3f
David_Martinez <davidmartinez5732891@gmail.com>
2025-06-13 11:20:24 -0600

    fue instalado ant design para agregar un carrucel, se actualizo tailwind, se agrego la etiqueta del carrucel en el home, además se creo el nuevo componente del carrucel

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,13 @@
 import Image from "next/image";
+import Carrucel from "./components/Carrucel.js";
 
 export default function Home() {
   return (
+    <>
+    <Carrucel />
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        
         <p className="text-2xl font-bold">
           Bienvenido a Nuestro Sitio Web
         </p>
@@ -22,5 +26,6 @@ export default function Home() {
         </p>
       </footer>
     </div>
+    </>
   );
 }
