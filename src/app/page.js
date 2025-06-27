01229e410771b0b9038498d2b44432bb38143895
David_Martinez <davidmartinez5732891@gmail.com>
2025-06-27 14:15:39 -0600

    Se actualizó la version de antd, se modifico totalmente el carrucel, se agregaron imagenes de prueba para testear el carrucel, y se verifico el funcionamiento correcto de todo

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
-import Image from "next/image";
 import Carrucel from "./components/Carrucel.js";
 
 export default function Home() {
   return (
     <>
+    <h1 className="grid text-sm sm:text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+      CALIDAD EN PRODUCTO
+    </h1>
     <Carrucel />
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
