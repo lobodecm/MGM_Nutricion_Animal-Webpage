b86e9f2fb35e47fcc633e0aba7510cb0b74438e6
David_Martinez <davidmartinez5732891@gmail.com>
2025-07-11 14:07:50 -0600

    se cambio la configuracion del carrucel de la pagina de queines somos, se hizo que tuviera una relacion de 16/9

--- src/app/components/Carrucelquienessomos.js
+++ src/app/components/Carrucelquienessomos.js
@@ -37,7 +37,7 @@ function useIsBigScreen() {
   return isBig;
 }
 
-export default function Carrucelquienessomos () {
+export default function Carrucelhome() {
   const isSmall = useIsSmallScreen();
   const isMedioum = useIsMedioumScreen();
   const isBig = useIsBigScreen();
@@ -53,20 +53,22 @@ export default function Carrucelquienessomos () {
         arrows={true}
         className={
           isSmall
-            ? "w-[90vw] h-[25vw] mx-auto rounded-2xl "
+            ? "w-full max-w-full mx-auto rounded-2xl"
           : isMedioum
-            ? "w-[90vw] h-[25vw] mx-auto rounded-2xl"
-          : isBig
-            ? "w-[90vw] h-[25vw] mx-auto rounded-2xl"
-            : "w-[90vw] h-[25vw] mx-auto rounded-2xl"
+            ? "w-[70%] mx-auto rounded-2xl"
+            : isBig
+            ? "w-[70%] mx-auto rounded-2xl"
+            : "w-[70%] mx-auto rounded-2xl"
         }
       >
         <div>
-          <img
-            src={isSmall ? "/images/quienessomos/Equipo.jpg" : isMedioum ? "/images/quienessomos/Equipo.jpg" : isBig ? "/images/quienessomos/Equipo.jpg" : "/images/quienessomos/Equipo.jpg"}
-            alt="Carousel Image 1 responsive"
-            className={isSmall ? "relative object-fill w-full h-[25vw] bg-[#364d79] mx-auto rounded-2xl" : isMedioum ? "relative object-fill w-full h-[25vw] bg-[#364d79] mx-auto rounded-2xl" : isBig ? "relative object-fill w-full h-[25vw] bg-[#364d79] mx-auto rounded-2xl" : "relative object-fill w-full h-[25vw] bg-[#364d79] mx-auto rounded-2xl"}
-          />
+          <div className="aspect-[16/9] w-full rounded-2xl overflow-hidden bg-[#364d79]">
+            <img
+              src={isSmall ? "/images/quienessomos/Equipo.jpg" : isMedioum ? "/images/quienessomos/Equipo.jpg" : isBig ? "/images/quienessomos/Equipo.jpg" : "/images/quienessomos/Equipo.jpg"}
+              alt="Imagen que hace referencia al equipo de trabajo"
+              className="w-full h-full object-cover"
+            />
+          </div>
         </div>
       </Carousel>
     </div>
